0c21481b5f6bf509d206dd5c19e5006d11e2cfe8
ankithasmohandas <ankithasmohandas@192.168.8.231>
2019-10-09 22:35:39 -0700

    Second version with complete code

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,9 +3,11 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private StringBuffer calculations;
 	
 	public AddingMachine () { //starts the program and appends 0
 		total = 0;  
+		calculations = new StringBuffer("0");
 	}
 	
 	public int getTotal () { //returns the total of the calculations
@@ -13,18 +15,18 @@ public class AddingMachine {
 	}
 	
 	public void add (int value) { //adds the given value to either 0 or other expressions
-		
+		total = total + value;
 	}
 	
 	public void subtract (int value) { //subtracts the given value to either 0 or other expressions
-		
+		total = total - value;
 	}
 
 	public String toString () { //converts expression to strings
-		return "";
+		return calculations.toString();
 	}
 
 	public void clear() { //clears all calculations and turns it into 0
-		
+		total = 0;
 	}
 }
