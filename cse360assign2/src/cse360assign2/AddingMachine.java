e5d2a2219073f5a0901dc3a2df6df561cfcf6441
ankithasmohandas <ankithasmohandas@192.168.8.231>
2019-10-09 22:33:00 -0700

    Javadoc comments of the methods

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -4,27 +4,27 @@ public class AddingMachine {
 
 	private int total;
 	
-	public AddingMachine () {
+	public AddingMachine () { //starts the program and appends 0
 		total = 0;  
 	}
 	
-	public int getTotal () { 
+	public int getTotal () { //returns the total of the calculations
 		return total;
 	}
 	
-	public void add (int value) { 
+	public void add (int value) { //adds the given value to either 0 or other expressions
 		
 	}
 	
-	public void subtract (int value) { 
+	public void subtract (int value) { //subtracts the given value to either 0 or other expressions
 		
 	}
 
-	public String toString () { 
+	public String toString () { //converts expression to strings
 		return "";
 	}
 
-	public void clear() { 
+	public void clear() { //clears all calculations and turns it into 0
 		
 	}
 }
