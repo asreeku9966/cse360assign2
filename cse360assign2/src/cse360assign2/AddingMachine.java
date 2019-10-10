f628253f323794106082d001e82533337125c6f7
ankithasmohandas <ankithasmohandas@192.168.8.231>
2019-10-09 22:26:46 -0700

    Inital

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  
+	}
+	
+	public int getTotal () { 
+		return total;
+	}
+	
+	public void add (int value) { 
+		
+	}
+	
+	public void subtract (int value) { 
+		
+	}
+
+	public String toString () { 
+		return "";
+	}
+
+	public void clear() { 
+		
+	}
+}
